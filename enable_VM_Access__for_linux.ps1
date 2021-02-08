df6b169d33caaac6be5a30995246f7b7e5d0e104
Adam <51384650+azach2878@users.noreply.github.com>
2021-02-08 09:08:58 -0700

    Delete enable_VM_Access__for_linux.ps1

--- enable_VM_Access__for_linux.ps1
+++
@@ -1,6 +0,0 @@
-﻿$VMSS         = 'aks-agentpool-11552782-vmss'
-$RG           = 'MC_AKS_AKS_EASTUS'
-$ExtName      = 'VMAccessForLinux'
-$Publisher    = 'Microsoft.OSTCExtensions'
-
-Add-AzVmssExtension -VirtualMachineScaleSet $VMSS -Name $ExtName -Publisher $Publisher -Type $ExtType -TypeHandlerVersion $ExtVer -AutoUpgradeMinorVersion $True
