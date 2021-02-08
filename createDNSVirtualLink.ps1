f2a4e604030f171cee97d6e4760916c2beb62de1
Adam <51384650+azach2878@users.noreply.github.com>
2021-02-08 09:06:35 -0700

    Update createDNSVirtualLink.ps1

--- createDNSVirtualLink.ps1
+++ createDNSVirtualLink.ps1
@@ -1,4 +1,6 @@
-You need to create a new Virtual Network Link in your Private DNS.
+# Run this script after creating your AKS cluster Node.
+# You need to create a new Virtual Network Link in your Private DNS in the AKS MC_ Resource group..... Get the DNS Name which was created when creating your aks. 
+
 $DNSZone        = "663eb315-5878-4381-95ad-405a329052b8.privatelink.eastus2.azmk8s.io"
 $DNSZoneRG      = "MC_aks_aks_eastus2"
 $TargetvNet     = Get-AzVirtualNetwork -Name hub-vnet
