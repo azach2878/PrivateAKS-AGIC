0a3cfce216603dae273cf235bfdfebf65c34aaae
Adam <51384650+azach2878@users.noreply.github.com>
2021-02-08 09:07:47 -0700

    Update enable-agic-addon.sh

--- enable-agic-addon.sh
+++ enable-agic-addon.sh
@@ -1,3 +1,4 @@
+# Note | Run this script after creating your AKS Cluster and you Application Gateway 
 appgwId=$(az network application-gateway show -n AKS-AppGateway-WAF2 -g aks -o tsv --query "id")
 echo $appgwId
 az aks enable-addons -n aks -g aks -a ingress-appgw --appgw-id $appgwId
